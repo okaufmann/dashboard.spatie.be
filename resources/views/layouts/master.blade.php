e05271893fcda6fe67c61b9f74f43f4a46125341
Oliver Kaufmann <okaufmann91@gmail.com>
2016-07-06 09:15:23 +0200

    Change Title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Dashboard</title>
+    <title>Dashboard</title>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,900' rel='stylesheet'
           type='text/css'>
     <link href="{{ elixir("css/app.css") }}" rel="stylesheet"/>
